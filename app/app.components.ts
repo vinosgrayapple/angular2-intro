fbcb605e6539b39fa0aa30dffb0fea1cb18c2e50
Сергей Комаричев < >
2016-11-24 00:32:41 +0200

    add Bindings

--- app/app.components.ts
+++ app/app.components.ts
@@ -1,9 +1,40 @@
 import  { Component } from '@angular/core';
+let todos = [
+    {
+        title: 'Выучить Angular2',
+        completed: true
+    }, 
+    {
+        title: 'Изучить TypeScript',
+        completed: false
+    }, 
+    {
+        title: 'Доделать Домашку',
+        completed: false
+    }, 
+    {
+        title: 'Здать Хвосты',
+        completed: false
+    }
+    ];
 @Component({
+    moduleId: module.id,
     selector: 'app',
-    template: `<h1>Angular2 Do</h1>`
+    templateUrl: 'app.component.html',
+    styleUrls: ['app.components.css']
 })
 
 export class AppComponent {
-
+    title = 'Angular 2Do';
+    todos = todos;
+    toggle(todo: any) {
+        todo.completed = !todo.completed;
+        console.log('Binding ', todo);
+    }
+    delete(todo:any) {
+        let index = this.todos.indexOf(todo);
+        if (index > -1) {
+            this.todos.splice(index,1);
+        }
+    }
 }
